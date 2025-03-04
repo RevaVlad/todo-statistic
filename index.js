24f829a656c8df56549ff340014083a83ce7fb2a
Andrey <Andreyvol9898@gmail.com>
2025-03-04 18:07:07 +0500

    start

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
+const path = require('path');
 
 const files = getFiles();
+const todos = extractTodos(files);
 
+// ToDo: sadlmfas
 console.log('Please, write your command!');
 readLine(processCommand);
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+    return filePaths.map(filePath => ({ content: readFile(filePath), fileName: path.basename(filePath) }));
 }
 
-function processCommand(command) {
-    switch (command) {
-        case 'exit':
-            process.exit(0);
-            break;
-        default:
-            console.log('wrong command');
-            break;
-    }
+function extractTodos(files) {
+    const todoRegex = /\/\/\s*TODO[:]?\s*(.*)/gi;
+    return files.flatMap(({ content, fileName }) =>
+        content.split('\n')
+            .map(line => {
+                const match = todoRegex.exec(line);
+                if (match) {
+                    return parseTodo(match[1], fileName);
+                }
+                return null;
+            })
+            .filter(todo => todo)
+    );
 }
 
-// TODO you can do it!
+
+
+function printTable(data) {
+    const headers = ['!', 'user', 'date', 'comment', 'file'];
+    const rows = data.map(todo => [
+        todo.important ? '!' : '',
+        todo.user.padEnd(10),
+        todo.date.padEnd(10),
+        todo.comment.padEnd(50),
+        todo.fileName.padEnd(20)
+    ]);
+    console.log(headers.join(' | '));
+    console.log('-'.repeat(100));
+    rows.forEach(row => console.log(row.join(' | ')));
+    console.log('-'.repeat(100));
+}
